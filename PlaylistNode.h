7f3823f3cbde0a89c3afd30dd22bb8f54ac327c0
Adeel Allawala <75290587+adeelallawala@users.noreply.github.com>
2023-12-07 13:46:07 -0600

    Update PlaylistNode.h
    minor changes

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,24 +1,29 @@
+/* Type your code here */
 #ifndef PLAYLISTNODE_H
 #define PLAYLISTNODE_H
+
 
 class PlaylistNode {
   public:
     PlaylistNode();
     PlaylistNode(string id, string name, string artist, int length);
+    
     string GetID();
     string GetSongName();
     string GetArtistName();
     int GetSongLength();
+    
     PlaylistNode* GetNext();
     void InsertAfter(PlaylistNode* nodePtr);
     void SetNext(PlaylistNode* nodePtr);
     void PrintPlaylistNode();
+    
   private:
     string uniqueID;
     string songName;
     string artistName;
     int songLength;
-    PlaylistNode* nextNodePtr;
+    PlaylistNode* nextNodePtr = nullptr;
 };
 
 #endif
